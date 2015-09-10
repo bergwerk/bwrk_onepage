cb7e5bd5a30cb1e40c9b20dcb4783c9401ef3e8e
Georg Dümmler <gd@bergwerk.ag>
2015-09-10 10:03:44 +0200

    Denpendencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,5 +1,32 @@
 <?php
 
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2015 Georg Dümmler <gd@bergwerk.ag>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ *
+ * @author	Georg Dümmler <gd@bergwerk.ag>
+ * @package	TYPO3
+ * @subpackage	bwrk_onepage
+ ***************************************************************/
+
 $EM_CONF[$_EXTKEY] = array(
     'title' => 'Onepage Extension',
     'description' => 'TYPO3 Onepage Extension for Pages',
@@ -15,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '2.1.0',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2',
+            'typo3' => '6.2.0-6.2.99',
         ),
         'conflicts' => array(
         ),
