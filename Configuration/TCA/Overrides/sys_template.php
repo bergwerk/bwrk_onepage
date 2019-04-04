10ac98874cd5e4210e643f3e449e2bd152b706a9
Henrik Ziegenhain <henrik.ziegenhain@multivisio.de>
2019-04-04 12:04:48 +0200

    [FEATURE] Add 9 LTS and drop 6 LTS compatibility

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,9 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+// Typoscript
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'bwrk_onepage',
+    'Configuration/TypoScript',
+    'BERGWERK Onepage [Viewer]'
+);
